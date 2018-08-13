d93d76db58eeb26dd740165e9aa1d66aa20d182f
byndasma <byndasma@gmail.com>
2018-08-13 17:10:33 -0400

    more modulation

--- js/knightLit.js
+++ js/knightLit.js
@@ -1,20 +1,7 @@
 import onBoardNonActiveIds from './onBoardNonActiveIds.js';
+import knightSpaces from './knightSpaces.js';
 
 // exports to possibleMoves
 export function knightLit() {
-
-    function knightSpaces(knight) { 
-        return [
-            (+knight.id[0] + 1) + (+knight.id[1] + 2).toString(),
-            (+knight.id[0] + 1) + (knight.id[1] - 2).toString(),
-            (knight.id[0] - 1) + (+knight.id[1] + 2).toString(),
-            (knight.id[0] - 1) + (knight.id[1] - 2).toString(),
-            (+knight.id[0] + 2) + (+knight.id[1] + 1).toString(),
-            (+knight.id[0] + 2) + (knight.id[1] - 1).toString(),
-            (knight.id[0] - 2) + (+knight.id[1] + 1).toString(),
-            (knight.id[0] - 2) + (knight.id[1] - 1).toString()
-        ];
-    } // knightLit helper
-
 	litIds = knightSpaces(pieceToMove).filter(onBoardNonActiveIds);
 } // fills litIds with ids where knight can move
