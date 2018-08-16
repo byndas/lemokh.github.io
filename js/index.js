8a08d56ddd42b47ed344b7fc284dd56b0b99cd34
byndasma <byndasma@gmail.com>
2018-08-16 12:10:10 -0400

    timer/set

--- js/index.js
+++ js/index.js
@@ -54,50 +54,52 @@ var blueNodes = board.querySelectorAll("[data-side='blue']"),
 ////////////////////////////////////////////////////////////
 
 window.onload = function () {
-	document.getElementById('start').addEventListener('click', function getMinutes() {
-	timerSet = document.getElementById('timeSet').value;	
-		if (timerSet) {
-			if (timerSet > 0) {
-				if (timerSet < 1000) {	
-					if (!timerSet.includes('.'))
-						if (!timerSet.includes('e')) {
-					
-						userInput = +(timerSet);
-						
-						clock1 = document.getElementById('time1');
-						clock1.innerHTML = userInput+':00';
-						
-						clock2 = document.getElementById('time2');
-						clock2.innerHTML = userInput+':00';
+	document.getElementById('start')
+			.addEventListener('click', function getMinutes() {
+				var timerSet = document.getElementById('timeSet').value;	
+				if (timerSet) {
+					if (timerSet > 0) {
+						if (timerSet < 1000) {	
+							if (!timerSet.includes('.'))
+								if (!timerSet.includes('e')) {
+							
+								userInput = +(timerSet);
+								
+								clock1 = document.getElementById('time1');
+								clock1.innerHTML = userInput+':00';
+								
+								clock2 = document.getElementById('time2');
+								clock2.innerHTML = userInput+':00';
 
-						blueTime = {
-							minutes: userInput,
-							tenths: 0,
-							hundredths: 0 
-						};
-						
-						orangeTime = {
-							minutes: userInput,
-							tenths: 0,
-							hundredths: 0 
-						};
+								blueTime = {
+									minutes: userInput,
+									tenths: 0,
+									hundredths: 0 
+								};
+								
+								orangeTime = {
+									minutes: userInput,
+									tenths: 0,
+									hundredths: 0 
+								};
 
-						document.getElementById('modal').style.display = "none";
-						
-						function showTimers(timer) {
-							timer.style.visibility = "visible";
-							timer.style.opacity = '1';
-							timer.style.transform = 'scale(1.0)';
-							timer.style.transition = 'visibility 0s linear 0s, opacity 0.25s 0s, transform 0.25s';
+								document.getElementById('modal').style.display = "none";
+								
+								function showTimers(timer) {
+									timer.style.visibility = "visible";
+									timer.style.opacity = '1';
+									timer.style.transform = 'scale(1.0)';
+									timer.style.transition = 'visibility 0s linear 0s, opacity 0.25s 0s, transform 0.25s';
+								}
+
+								showTimers(document.getElementById('time1'));
+								showTimers(document.getElementById('time2'));
+								
+								lit();
+							}
 						}
-
-						showTimers(document.getElementById('time1'));
-						showTimers(document.getElementById('time2'));
-						
-						lit();
 					}
 				}
 			}
-		}
-	});
+	);
 }
