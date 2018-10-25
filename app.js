881bfe194c7209a3d1760a35622dee6050547a05
byndasma <byndasma@gmail.com>
2018-10-24 20:53:24 -0400

    update

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ app.use(express.static('public'));
 var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
+// setup my socket server
+var io = require('socket.io')(http);
+ 
+io.on('connection', function(socket) {
+    console.log('new connection');
+
+    socket.on('message', function(msg) {
+        console.log('Got message from client: ' + msg);     
+    });
+
+    // Called when the client calls socket.emit('move')
+    // socket.on('move', function(msg) {
+    //    socket.broadcast.emit('move', msg); 
+    // });
+});
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/default.html');
 });
@@ -12,18 +28,3 @@ http.listen(port, function() {
     console.log('listening on *: ' + port);
 });
 
-// setup my socket server
-var io = require('socket.io')(http);
- 
-io.on('connection', function(socket) {
-    console.log('new connection');
-
-    // socket.on('message', function(msg) {
-    //     console.log('Got message from client: ' + msg);     
-    // });
-
-    // Called when the client calls socket.emit('move')
-    socket.on('move', function(msg) {
-       socket.broadcast.emit('move', msg); 
-    });
-});
--- public/default.js
+++ public/default.js
@@ -1,15 +1,12 @@
-// setup my socket client
+// setup socket client
 var socket = io();
 
-// called when a player makes a move on the board UI
-var handleMove = function(source, target) {
-    var move = [ [x1,y1], [x2,y2] ];
-    // if (move === null)  return 'snapback';
-    socket.emit('move', move);
+// called when player moves piece
+var handleMove = function(firstClick, secondClick) {
+    socket.emit('move', [firstClickId,  secondClickId]);
 };
 
 // called when the server calls socket.broadcast('move')
 socket.on('move', function (msg) {
-    // trigger the two clicks [ [x1,y1], [x2,y2] ]
-    // on the waiting player's html
+    // trigger both clicks in the waiting player's main.js
 });
